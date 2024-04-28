fe50032c2639cdbc52f1bd948670ed75c8b81e4d
Caceres16 <167395736+Caceres16@users.noreply.github.com>
2024-04-27 22:06:27 -0600

    Add files via upload

--- demoTF/src/main/java/pe/edu/upc/demotf/Controllers/BankingEntityController.java
+++ demoTF/src/main/java/pe/edu/upc/demotf/Controllers/BankingEntityController.java
@@ -1,42 +1,46 @@
-package pe.edu.upc.demotf.Controllers;
+package pe.edu.upc.demotf.controllers;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.demotf.Dtos.BankingEntityDTO;
-import pe.edu.upc.demotf.Entities.BankingEntitys;
-import pe.edu.upc.demotf.ServicesInterfaces.IBankingEntityServices;
+import pe.edu.upc.demotf.dtos.BankingEntityDTO;
+import pe.edu.upc.demotf.entities.BankingEntity;
+import pe.edu.upc.demotf.servicesinterfaces.IAccountBankingService;
+import pe.edu.upc.demotf.servicesinterfaces.IBankingEntityService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/bankingentity")
+@RequestMapping("/entidades")
 public class BankingEntityController {
-@Autowired
-    private IBankingEntityServices bS;
-@PostMapping
-    public  void registrar (@RequestBody BankingEntityDTO b){
-    ModelMapper m = new ModelMapper();
-    BankingEntitys be = m.map(b,BankingEntitys.class);
-    bS.insert((be));
-}
-@GetMapping
+    @Autowired
+    private IBankingEntityService bE;
+    @PostMapping
+    public void registrar(@RequestBody BankingEntityDTO b){
+        ModelMapper m=new ModelMapper();
+        BankingEntity be=m.map(b,BankingEntity.class);
+        bE.insert(be);
+    }
+    @PutMapping
+    public void modificar(@RequestBody BankingEntityDTO b){
+        ModelMapper m=new ModelMapper();
+        BankingEntity be=m.map(b,BankingEntity.class);
+        bE.insert(be);
+    }
+    @GetMapping
     public List<BankingEntityDTO> list(){
-    return bS.list().stream().map(y-> {
-        ModelMapper m = new ModelMapper();
-        return m.map(y, BankingEntityDTO.class);
-    }).collect(Collectors.toList());
-}
-@DeleteMapping("/{id}")
-public  void  eliminar ( @PathVariable("id") Integer id){
-    bS.delete(id);
-}
-@GetMapping("/{id}")
-public  BankingEntityDTO listarId(@PathVariable("id") Integer id){
-        ModelMapper m = new ModelMapper();
-        BankingEntityDTO dto =m.map(bS.listId(id), BankingEntityDTO.class);
-        return  dto;
+        return bE.list().stream().map(y->{
+            ModelMapper m=new ModelMapper();
+            return m.map(y,BankingEntityDTO.class);
+        }).collect(Collectors.toList());
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id")Integer id){bE.delete(id);}
+
+    public BankingEntityDTO listarId(@PathVariable("id") Integer id){
+        ModelMapper m=new ModelMapper();
+        BankingEntityDTO dto=m.map(bE.lisId(id),BankingEntityDTO.class);
+        return dto;
     }
 }
