fcd3ee5fb9e2ebf5723a2e67821041cf315cdda6
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-04-28 00:34:04 -0500

    Delete demoTF directory

--- demoTF/src/main/java/pe/edu/upc/demotf/repositories/IBankingEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.demotf.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demotf.entities.BankingEntity;
-
-@Repository
-public interface IBankingEntityRepository extends JpaRepository<BankingEntity,Integer> {
-}
